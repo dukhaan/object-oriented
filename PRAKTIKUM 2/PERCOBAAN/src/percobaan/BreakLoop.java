a31aa302da64835ebadc0d4f17038eb4049b5f19
Dukhaan <dukhaank@gmail.com>
2023-08-28 20:47:11 +0700

    Updated PRAKTIKUM 2

---
+++ PRAKTIKUM 2/PERCOBAAN/src/percobaan/BreakLoop.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package percobaan;
+
+/**
+ *
+ * @author Dukhaan
+ */
+public class BreakLoop {
+
+    public static void main(String args[]) {
+        int i = 0;
+        do {
+            System.out.println("Iterasi ke " + i);
+            i++;
+            if (i > 10) {
+                break;
+            }
+        } while (true);
+    }
+}
---
+++ PRAKTIKUM 2/PERCOBAAN/src/percobaan/PERCOBAAN.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package percobaan;
+
+/**
+ *
+ * @author Dukhaan
+ */
+public class PERCOBAAN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
